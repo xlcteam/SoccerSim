fe5a61dfad45a8fc20ccdaedcbcb9de59fc167f1
mr.Shu <mr@shu.io>
2013-07-14 00:12:02 +0200

    let's just keep it simple

--- soccersim/main.js
+++ soccersim/main.js
@@ -6,10 +6,10 @@ gamejs.preload(['img/field.png']);
 gamejs.ready(function() {
 
     this.width = 729;
-    this.height = 646;
+    this.height = 546;
 
-    var display = gamejs.display.setMode([this.width, this.height], 
-        gamejs.display.DISABLESMOOTHING | gamejs.display.FULLSCREEN);
+    var display = gamejs.display.setMode([this.width, this.height]) //, 
+        /*gamejs.display.DISABLESMOOTHING | gamejs.display.FULLSCREEN); */
   //display.blit(
   //    (new gamejs.font.Font('30px Sans-serif')).render('Hello World')
   //);
@@ -19,10 +19,10 @@ gamejs.ready(function() {
     display.fill("#047a01");
     this.field = gamejs.image.load("img/field.png");
 
-    display.blit(this.field, [(this.width-729)/2, 100+(this.height-100-546)/2]);
+    display.blit(this.field, [(this.width-729)/2, (this.height-546)/2]);
     
-    var rect = new gamejs.Rect([0, 0], [this.width, 50]);
-    draw.rect(display, "#FFFFFF", rect, 100);
+  //var rect = new gamejs.Rect([0, 0], [this.width, 50]);
+  //draw.rect(display, "#FFFFFF", rect, 100);
 
 
     gamejs.onEvent(function(event) {
