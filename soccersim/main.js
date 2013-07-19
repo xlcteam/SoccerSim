141aa350e0b95d0630f642fd3db6ffdd9c47c1bb
mr.Shu <mr@shu.io>
2013-07-19 09:01:24 +0200

    BoxProp

--- soccersim/main.js
+++ soccersim/main.js
@@ -2,6 +2,8 @@ var gamejs = require('gamejs');
 var draw = require('gamejs/draw');
 var box2d = require('./Box2dWeb-2.1.a.3');
 
+var BoxProp = require('./Box2dWeb-2.1.a.3').BoxProp;
+
 var Robot = require('robot').Robot;
 var Ball = require('ball').Ball;
 
