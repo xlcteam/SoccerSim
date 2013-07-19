6993a0a82a3aaecbeab65ec5ce5e114a58c36138
mr.Shu <mr@shu.io>
2013-07-19 09:00:40 +0200

    objects

---
+++ soccersim/objects.js
@@ -0,0 +1,27 @@
+var box2d = require('./Box2dWeb-2.1.a.3');
+
+var BoxProp = function(pars){
+    /*
+   static rectangle shaped prop
+     
+     pars:
+     size - array [width, height]
+     position - array [x, y], in world meters, of center
+    */
+    this.size=pars.size;
+    
+    //initialize body
+    var bdef=new box2d.b2BodyDef();
+    bdef.position=new box2d.b2Vec2(pars.position[0], pars.position[1]);
+    bdef.angle=0;
+    bdef.fixedRotation=true;
+    this.body=b2world.CreateBody(bdef);
+    
+    //initialize shape
+    var fixdef=new box2d.b2FixtureDef;
+    fixdef.shape=new box2d.b2PolygonShape();
+    fixdef.shape.SetAsBox(this.size[0]/2, this.size[1]/2);
+    fixdef.restitution=0.4; //positively bouncy!
+    this.body.CreateFixture(fixdef);
+    return this;  
+};
