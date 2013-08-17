757f42840ab4c0f34606f8445bcb132161748d8b
Ado Matejov <a.matejov@centrum.sk>
2013-08-17 15:50:49 +0200

    started on timer

--- soccersim/timer.js
+++ soccersim/timer.js
@@ -2,7 +2,27 @@ var Timer = function(mins, secs, millis){
     this.mins = mins;
     this.secs = secs;
     this.millis = millis;
+
+    this.startTime = [this.mins, this.secs, this.millis];
 };
+
+Timer.prototype.start = function (){
+    this.millis -= 0.5;
+
+    if (this.millis < 0){
+        this.millis = 0;
+        this.secs--;
+        if (this.secs < 0){
+            this.mins--;
+            if (this.mins < 0){
+                this.mins = this.secs = this.millis = 0;
+                return;
+            }
+        }
+    }
+
+    setInterval(this.start(), 50);
+}
 
 Timer.prototype.returnTime = function(){
     return [mins, secs, millis];
