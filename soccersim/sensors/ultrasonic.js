5b1d815f7d5b94f4e54c4a42d71ce036d2aa7684
mr.Shu <mr@shu.io>
2013-09-06 17:39:53 +0200

    US sensor drawing

--- soccersim/sensors/ultrasonic.js
+++ soccersim/sensors/ultrasonic.js
@@ -1,9 +1,21 @@
+var gamejs = require('gamejs');
+var draw = require('gamejs/draw');
 
-var UltraSonicSensor = function(robot, position, angle) {
+var UltraSonicSensor = function(robot, pos, angle) {
     this.robot = robot;
+    this.pos = pos;
+    this.x = this.robot.rect.left + this.pos[0];
+    this.y = this.robot.rect.top + this.pos[1]
+
 };
 
-UltraSonicSensor.prototype.get_angle = function() {
+UltraSonicSensor.prototype.draw = function(surface) {
+    this.x = this.robot.rect.left + this.pos[0];
+    this.y = this.robot.rect.top + this.pos[1];
+    draw.circle(surface, "#AA0CB7", [this.x, this.y], 3, 0);
+}
+
+UltraSonicSensor.prototype.read = function() {
 
 }
 
