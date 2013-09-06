f4bfdbb765a6bf8e4929e7c6a85752fa9ef8f53c
mr.Shu <mr@shu.io>
2013-09-06 17:34:45 +0200

    read prototype

--- soccersim/sensors/light.js
+++ soccersim/sensors/light.js
@@ -6,13 +6,16 @@ var LightSensor = function(robot, pos){
     this.pos = pos;
     this.x = this.robot.rect.left + this.pos[0];
     this.y = this.robot.rect.top + this.pos[1];
-    this.lightness;
 };
 
 LightSensor.prototype.draw = function (surface){
     this.x = this.robot.rect.left + this.pos[0];
     this.y = this.robot.rect.top + this.pos[1];
     draw.circle(surface, "#000CB7", [this.x, this.y], 3, 0);
+}
+
+LightSensor.prototype.read = function() {
+
 }
 
 exports.LightSensor = LightSensor;
