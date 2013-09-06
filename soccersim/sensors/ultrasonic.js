8b255aff75932b27651705ae24812fdb156f9ce9
mr.Shu <mr@shu.io>
2013-09-06 17:27:33 +0200

    ultrasonic started

---
+++ soccersim/sensors/ultrasonic.js
@@ -0,0 +1,10 @@
+
+var UltraSonicSensor = function(robot, position, angle) {
+    this.robot = robot;
+};
+
+UltraSonicSensor.prototype.get_angle = function() {
+
+}
+
+exports.UltraSonicSensor = UltraSonicSensor
