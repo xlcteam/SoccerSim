1c036c7a2ff465c2ca975ba75d05870b2bb8f960
mr.Shu <mr@shu.io>
2013-10-06 23:59:36 +0200

    basic movement API for the dummy

--- soccersim/dummy_robot.js
+++ soccersim/dummy_robot.js
@@ -14,9 +14,26 @@ DummyRobot.prototype.stop = function () {
     return this.send_command('stop()');
 }
 
+DummyRobot.prototype.forward = function (speed) {
+    return this.send_command('forward(' + speed + ')');
+}
+
+DummyRobot.prototype.reverse = function (speed) {
+    return this.send_command('reverse(' + speed + ')');
+}
+
+DummyRobot.prototype.right = function (speed) {
+    return this.send_command('right(' + speed + ')');
+}
+
+DummyRobot.prototype.left = function (speed) {
+    return this.send_command('left(' + speed + ')');
+}
+
 DummyRobot.prototype.send_command = function (command) {
     return gamejs.worker.post({code: this.id + '.' + command + ';' });
 }
+
 
 
 exports.DummyRobot = DummyRobot;
